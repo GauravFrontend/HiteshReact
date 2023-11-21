0261a6c5f09fe85702cfcaea2d2571e545df0bcf
GauravFrontend <gaurav9911093928@gmail.com>
2023-11-21 08:35:03 +0530

    React Router Dom

---
+++ 05ReactRouter/src/main.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Contact from "./components/Contact.jsx";
+import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>,
+    children:[
+      {
+        path:"",
+        element:<Home/>
+      },
+      {
+        path:"contact",
+        element:<Contact/>
+      }
+      ,
+      {
+        path:"about",
+        element:<About/>
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
